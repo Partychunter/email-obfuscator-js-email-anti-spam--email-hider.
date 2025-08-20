44616886be889ac25030b8b0c30ca63e64a8ab1b
Partychunter <110786297+Partychunter@users.noreply.github.com>
2025-08-20 23:26:22 +0200

    Add files via upload

---
+++ email-obfuscator-display-email.js
@@ -0,0 +1,5 @@
+<script LANGUAGE="JavaScript">"user = "user";
+site = "Domin";
+
+document.write('<a href="mailto:' + user + '@' + site + '">');
+document.write(user + '@' + site + '</a>'); </SCRIPT>
---
+++ mail-obfuscator-text-link.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<a id="kontaktLink" href="#"> Text.</a>
+
+<script LANGUAGE="JavaScript">
+var user = "username";
+var site = "Domin";
+var emailLink = "mailto:" + user + "@" + site;
+var kontaktLink = document.getElementById("kontaktLink");
+kontaktLink.href = emailLink;
+</script>
+</html>
